9619a4b3558a0a2f36fd23dd6359f3cf95fdb686
Luciano Ricotta <lucianoricotta@fedora.attlocal.net>
2025-12-26 08:25:41 -0600

    remove __tests__ folder

--- server/src/__tests__/setup.ts
+++
@@ -1,8 +0,0 @@
-// Test setup file
-process.env.NODE_ENV = 'test';
-process.env.JWT_SECRET = 'test-secret-key-for-testing';
-process.env.JWT_EXPIRES_IN = '1h';
-
-// Silence console during tests
-jest.spyOn(console, 'log').mockImplementation(() => {});
-jest.spyOn(console, 'error').mockImplementation(() => {});
